8c4a17f5b18901d2bae48764b1ede040373c0591
suba <subametha21@gmail.com>
2019-08-14 10:10:50 +0530

    src added

---
+++ src/main/java/day3/Valid.java
@@ -0,0 +1,16 @@
+package day3;
+
+public class Valid {
+	public static int check()
+	{
+	if(name==null||name.trim()=="")
+	{
+		System.out.println("Invalid name");
+	}
+	else
+	{
+		System.out.println("Valid name");
+	}
+
+}
+}
---
+++ src/test/java/day3/Valid.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class Valid {
+
+	public static void main(String[] args) {
+		String="suba";
+		int a=Valid.check();
+		
+	}
+
+}
